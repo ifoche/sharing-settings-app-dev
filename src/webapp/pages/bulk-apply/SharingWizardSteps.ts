bb3f68f0f692db48968deebfefaf694a5e9f992b
Alexis Rico <sferadev@gmail.com>
2021-09-08 09:43:16 +0200

    Remove descriptions

--- src/webapp/pages/bulk-apply/SharingWizardSteps.ts
+++ src/webapp/pages/bulk-apply/SharingWizardSteps.ts
@@ -32,27 +32,23 @@ export const metadataSharingWizardSteps: MetadataSharingWizardStep[] = [
         label: i18n.t("Select Metadata"),
         component: SelectMetadataStep,
         validationKeys: ["selection"],
-        description: i18n.t("Select metadata to share"),
     },
     {
         key: "list-dependencies",
         label: i18n.t("List Dependencies and Exclude"),
         component: ListDependenciesStep,
         validationKeys: ["name"],
-        description: i18n.t("List Dependencies and Exclude"),
     },
     {
         key: "sharing-access",
         label: i18n.t("Sharing Access"),
         component: AccessStep,
         validationKeys: ["name"],
-        description: i18n.t("Sharing Access"),
     },
     {
         key: "summary-apply",
         label: i18n.t("Summary and Apply Metadata"),
         component: SummaryApplyStep,
         validationKeys: ["name"],
-        description: i18n.t("List errors and warnings and apply metadata sharing"),
     },
 ];
