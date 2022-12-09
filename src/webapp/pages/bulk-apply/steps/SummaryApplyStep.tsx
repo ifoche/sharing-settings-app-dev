1fe846a2fe06d64b9686a22dfb044a888e7ad837
deeonwuli <onwulidumebi@gmail.com>
2022-12-09 12:58:42 +0100

    show warning when public access is changed

--- src/webapp/pages/bulk-apply/steps/SummaryApplyStep.tsx
+++ src/webapp/pages/bulk-apply/steps/SummaryApplyStep.tsx
@@ -1,7 +1,8 @@
-import { useSnackbar } from "@eyeseetea/d2-ui-components";
+import { ConfirmationDialog, useSnackbar } from "@eyeseetea/d2-ui-components";
 import Button from "@material-ui/core/Button";
 import React, { useCallback, useState } from "react";
 import { ImportResult } from "../../../../domain/entities/ImportResult";
+import i18n from "../../../../locales";
 import { ImportSummary } from "../../../components/import-summary/ImportSummary";
 import { useAppContext } from "../../../contexts/app-context";
 import { MetadataSharingWizardStepProps } from "../SharingWizardSteps";
@@ -11,6 +12,7 @@ export const SummaryApplyStep: React.FC<MetadataSharingWizardStepProps> = ({ bui
     const snackbar = useSnackbar();
 
     const [importResult, setImportResult] = useState<ImportResult>();
+    const [openDialog, setDialogOpen] = useState<boolean>(false);
 
     const applySharingSync = useCallback(() => {
         compositionRoot.metadata
@@ -26,7 +28,24 @@ export const SummaryApplyStep: React.FC<MetadataSharingWizardStepProps> = ({ bui
         <React.Fragment>
             {importResult && <ImportSummary results={[importResult]} onClose={() => setImportResult(undefined)} />}
 
-            <Button variant="contained" color="primary" onClick={applySharingSync}>
+            <ConfirmationDialog
+                isOpen={openDialog}
+                title={i18n.t("Warning")}
+                onCancel={() => setDialogOpen(false)}
+                onSave={applySharingSync}
+                saveText={i18n.t("Continue")}
+                cancelText={i18n.t("Go back")}
+                maxWidth={"sm"}
+                fullWidth={true}
+            >
+                You are about to change the public access sharing setting. Would you like to continue?
+            </ConfirmationDialog>
+
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => (builder.sharings.publicAccess !== "--------" ? setDialogOpen(true) : applySharingSync)}
+            >
                 Apply Sharing Settings
             </Button>
         </React.Fragment>
